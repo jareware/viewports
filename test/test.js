4d93f37c74cc0f5f4154b88d5c63b087758887ff
Jarno Rantanen <jarno@jrw.fi>
2013-03-13 22:14:51 +0200

    Added the first fixture-based test.

--- test/test.js
+++ test/test.js
@@ -1,10 +1,26 @@
 var assert = require('assert');
+var fs = require('fs');
+var child_process = require('child_process');
 
-describe('Array', function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
-            assert.equal(-1, [1, 2, 3].indexOf(5));
-            assert.equal(-1, [1, 2, 3].indexOf(0));
+describe('viewport.scss', function() {
+
+    it('should produce a most basic ranged media query', function(done) {
+
+        fs.readFile('test/fixtures/barebones.css', 'utf8', function(err, expected) {
+
+            if (err)
+                assert.fail(err, 'Could not read expected CSS file');
+
+            child_process.exec('sass --style expanded test/fixtures/barebones.scss', function callback(err, actual) {
+
+                if (err)
+                    assert.fail(err, 'Could not compile actual SCSS file');
+
+                assert.equal(expected, actual);
+
+                done();
+
+            });
         });
     });
 });
