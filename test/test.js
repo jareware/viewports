e75ebf5db318b49431d31a75070da402c0e76652
Jarno Rantanen <jarno@jrw.fi>
2013-08-24 17:43:41 +0300

    Renamed the spec path.

--- test/test.js
+++ test/test.js
@@ -2,28 +2,30 @@ var assert = require('assert');
 var fs = require('fs');
 var child_process = require('child_process');
 
+var SPEC_FILE_PATH = 'test/spec';
 var SPEC_FILE_FORMAT = /^([\s\S]*)\/\* it (.+) \*\/([\s\S]*)\/\* should equal \*\/([\s\S]*)$/m;
 
 describe('viewports.scss', function() {
 
-    var files = fs.readdirSync('test/fixtures');
+    var files = fs.readdirSync(SPEC_FILE_PATH);
 
     files.forEach(function(file) {
 
         if (!file.match(/\.scss$/))
             return; // we're only interested in SCSS files
 
-        var contents = fs.readFileSync('test/fixtures/' + file, 'utf8');
+        var contents = fs.readFileSync(SPEC_FILE_PATH + '/' + file, 'utf8');
         var sections = contents.match(SPEC_FILE_FORMAT);
 
         if (!sections)
             assert.fail(file + ': File does not appear to follow the spec file format');
 
         var description = sections[2].trim();
+        var cmd = 'sass --style expanded ' + SPEC_FILE_PATH + '/' + file;
 
         it(description, function(done) {
 
-            child_process.exec('sass --style expanded test/fixtures/' + file, function(err, output, stdErr) {
+            child_process.exec(cmd, function(err, output, stdErr) {
 
                 if (err)
                     assert.fail(file + ': Could not compile: ' + stdErr);
